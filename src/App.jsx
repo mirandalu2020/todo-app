cb7a43d1ec976d3a09c0e2ef561566f10c58f87c
mirandalu2020 <yuanling1007@hotmail.com>
2023-05-17 11:23:20 -0700

    mnavbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Nav from './Components/Nav/nav'
 import Todo from './Components/Todo';
 import SettingsForm from './Components/SettingsForm';
 import SettingProvider from './Context/Settings';
@@ -8,10 +10,17 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <SettingProvider>
-        <Todo />
-        <SettingsForm />
-      </SettingProvider>
+      <>
+      <Nav />
+      <BrowserRouter>
+        <SettingProvider>
+          <Routes>
+            <Route path="/" element={<Todo />} />
+            <Route path="/settings" element={<SettingsForm />} />
+          </Routes>
+        </SettingProvider>
+      </BrowserRouter>
+      </>
     );
   }
 }
---
+++ src/Components/Nav/nav.js
@@ -0,0 +1,14 @@
+import './nav.scss'
+
+function Nav() {
+
+  return (
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/settings">Settings</a></li>
+  </ul>
+
+  )
+}
+
+export default Nav;
